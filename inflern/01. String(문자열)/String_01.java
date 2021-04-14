8ecbae507ac292d739f462bfef5a11c5d9c4eacc
nyju <nyju2104@gmail.com>
2021-04-15 01:02:43 +0900

    문자찾기

--- inflern/01. String(문자열)/String_01.java
+++ inflern/01. String(문자열)/String_01.java
@@ -2,17 +2,9 @@ package string;
 
 import java.util.Scanner;
 
-public class string_01 {
+public class String_01 {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String value1 = in.next(); // 문자열
-        if (value1.length() > 100) {
-            return;
-        }
-        String value2 = in.next(); // 문자
-
-
+    public int solution(String value1, String value2){
         int cnt = 0;
         int length = value1.length();
 
@@ -21,8 +13,19 @@ public class string_01 {
                 cnt++;
             }
         }
+        return cnt;
+    }
 
-        System.out.println(cnt);
+    public static void main(String[] args) {
+        String_01 T= new String_01();
+        Scanner in = new Scanner(System.in);
+        String value1 = in.next(); // 문자열
+        if (value1.length() > 100) {
+            return;
+        }
+        String value2 = in.next(); // 문자
+
+        System.out.println(T.solution(value1, value2));
         return;
     }
 

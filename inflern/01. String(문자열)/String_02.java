c3e115fbc33859cfcfe78467794c5fda0089a6c3
nyju <nyju2104@gmail.com>
2021-04-15 00:55:05 +0900

    알고리즘 문제풀기

---
+++ inflern/01. String(문자열)/String_02.java
@@ -0,0 +1,30 @@
+package string;
+
+import java.util.Scanner;
+
+public class String_02 {
+
+    public void solution(String value1) {
+
+        for (int i = 0; i < value1.length(); i++) {
+            char ch = value1.charAt(i);
+
+            if (Character.isUpperCase(ch) == true) {
+                System.out.print(Character.toString(ch).toLowerCase());
+            } else {
+                System.out.print(Character.toString(ch).toUpperCase());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String_02 T = new String_02();
+        Scanner in = new Scanner(System.in);
+
+        String value1 = in.next();
+
+        T.solution(value1);
+        return;
+    }
+
+}
